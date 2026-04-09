fab4fccd092f964393a34a481a8a8e487c6e35a6
lqphu2005 <lqphu369@gmail.com>
2026-04-09 17:05:09 +0700

    feat: setup base template SB Admin 2

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Creative Agency Hub</title>
+
+    <!-- Font Awesome -->
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <!-- SB Admin 2 CSS -->
+    <link href="assets/css/sb-admin-2.css" rel="stylesheet">
+</head>
+
+<body id="page-top">
+
+    <!-- Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php include('components/sidebar.php'); ?>
+        <!-- End Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Navbar -->
+                <?php include('components/navbar.php'); ?>
+                <!-- End Navbar -->
+
+                <!-- Page Content -->
+                <div class="container-fluid">
+                    <h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
+                    <p>from mixi with love</p>
+                </div>
+                <!-- End Page Content -->
+
+            </div>
+            <!-- End Main Content -->
+
+            <!-- Footer -->
+            <?php include('components/footer.php'); ?>
+            <!-- End Footer -->
+
+        </div>
+        <!-- End Content Wrapper -->
+
+    </div>
+    <!-- End Wrapper -->
+
+    <!-- jQuery -->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- SB Admin 2 JS -->
+    <script src="assets/js/sb-admin-2.min.js"></script>
+
+</body>
+</html>
