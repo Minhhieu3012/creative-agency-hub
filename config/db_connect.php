8fe89d851b60ffa6105b12127da12be7b7a98bc8
Do Tien <tiendt4180@ut.edu.vn>
2026-04-23 21:42:13 +0700

    config với db trên máy

---
+++ config/db_connect.php
@@ -0,0 +1,16 @@
+<?php
+
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+
+$host = 'localhost';
+$dbname = 'creative_agency';
+$username = 'root';
+$password = ''; 
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Lỗi kết nối CSDL: " . $e->getMessage());
+}
+?>
