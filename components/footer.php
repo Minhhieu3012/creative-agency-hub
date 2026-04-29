4a3696f0ff64bef28bfa7dab52b086f23b0ba274
Minhhieu3012 <mjnkhieu3012@gmail.com>
2026-04-30 00:24:25 +0700

    delete components folder outside app folder

--- components/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-            <span>Creative Agency Hub &copy; 2026 - Nhom 08</span>
-        </div>
-    </div>
-</footer>
